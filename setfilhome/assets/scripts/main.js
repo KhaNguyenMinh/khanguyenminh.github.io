a98fcd7537a5a3df4ec2cd1ba7bd5ea3a553bf10
Kha Nguyen <mr.kha63@gmail.com>
2017-05-05 15:43:41 +0700

    Setfilhome

--- setfilhome/assets/scripts/main.js
+++ setfilhome/assets/scripts/main.js
@@ -181,20 +181,20 @@
         }
 
         /**end*************PRODUCT PAGE***************/
-        $('#productsPage .products-img .img-wrapper').click(function (e) {
-            var index = $(e.target).data('index');
-            var relatedIndex = $(e.target).data('related').split('-');
-
-            // $('#productsPage .products-img .img-wrapper').removeClass('hidden');
-            $('#productsPage .products-img .img-wrapper').css('width', '25%');
-            for (var i=0; i < relatedIndex.length; i++) {
-                var item = parseInt(relatedIndex[i]);
-                // $('#productsPage .products-img .img-wrapper[data-index="' + item + '"]').addClass('hidden');
-            }
-
-            $('#productsPage .products-img .img-wrapper[data-index="' + index + '"]').css('width', '50%');
-            console.log('index', index);
-        });
+        // $('#productsPage .products-img .img-wrapper').click(function (e) {
+        //     var index = $(e.target).data('index');
+        //     var relatedIndex = $(e.target).data('related').split('-');
+        //
+        //     // $('#productsPage .products-img .img-wrapper').removeClass('hidden');
+        //     $('#productsPage .products-img .img-wrapper').css('width', '25%');
+        //     for (var i=0; i < relatedIndex.length; i++) {
+        //         var item = parseInt(relatedIndex[i]);
+        //         // $('#productsPage .products-img .img-wrapper[data-index="' + item + '"]').addClass('hidden');
+        //     }
+        //
+        //     $('#productsPage .products-img .img-wrapper[data-index="' + index + '"]').css('width', '50%');
+        //     console.log('index', index);
+        // });
         /**start*************HOME PAGE***************/
 
         /**end*************PRODUCT PAGE***************/
